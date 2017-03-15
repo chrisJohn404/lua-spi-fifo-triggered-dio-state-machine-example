aebbd6345e295c2865f1375569c1e21a03a2372d
Chris Johnson <chris.s.johnson4@gmail.com>
2017-03-15 17:24:35 -0600

    Got script working.  Switches states on input triggers.  IO line is currently heavily de-bounced to allow for wire-based triggering.

--- fio-spi-fifo-v1.lua
+++ fio-spi-fifo-v1.lua
@@ -19,8 +19,8 @@ acqStateWriteData = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
 
 --SPI Slave device bytes to Read
 acqStateNumBytesToRead = 9
-dummyReadData = {}
-for i in acqStateNumBytesToRead do
+dummyWriteDataForRead = {}
+for i=1,acqStateNumBytesToRead do
   dummyWriteDataForRead[i]=0
 end
 
@@ -38,60 +38,94 @@ end
 --3: Wait for Trigger Re-Set. Reads FIO0, if: 
 --   1, Remains in the wait for trigger re-set state.
 --   0, Switches to the waiting for trigger state.
-state=1
+state=0
 
 INIT_STATE = 0
 WAIT_TRIG_STATE = 1
 ACQ_STATE = 2
 TRIG_RESET_STATE = 3
 
+--fio0Debouncing:
+numReq = 500
+curNum = 0
+
 FIO1_INIT_STATE = 0
 MB.W(2001,0,FIO1_INIT_STATE)
 
+oldState = 0
+numBetweenStatusUpdate = 50000
+curNoPrintCount = 0
+
 while true do
-if state == INIT_STATE then
-  -- Initializing
-  -- 1. Send 6 bytes of data to a SPI slave device.
-  MB.W(5009,0,table.getn(acqStartWriteData))
-  MB.WA(5010,99,table.getn(configBytes),configBytes)
-  MB.W(5007,0,1)
-
-  --Start waiting for a trigger to occur.
-  state = WAIT_TRIG_STATE
-elseif state = WAIT_TRIG_STATE then
-  -- Waiting for trigger
-  local fio0 = MB.R(2000, 0)
-  if fio0 == 1 then
-    --Switch to the acquisitino state.
-    state = ACQ_STATE
+  -- Debugging Code
+  if oldState ~= state then
+    oldState = state
+   print('Switching from', oldState, 'to: ', state)
+  else
+    curNoPrintCount=curNoPrintCount+1
+    if curNoPrintCount >= numBetweenStatusUpdate then
+      curNoPrintCount = 0
+      print('Current State', state)
+    end
   end
-elseif state = ACQ_STATE then
-  -- Acquire data from SPI device
-  -- 1. Send 6 bytes of data to a SPI slave device.
-  MB.W(5009,0,table.getn(acqStartWriteData))
-  MB.WA(5010,99,table.getn(acqStartWriteData),acqStartWriteData)
-  MB.W(5007,0,1)
 
-  -- 2. Read 9 bytes of data from a SPI slave device.
-  MB.W(5009,0,acqStateNumBytesToRead)
-  MB.WA(5010,99,acqStateNumBytesToRead,dummyWriteDataForRead)
-  MB.W(5007,0,1)
-  local readData = MB.RA(5050, 99)
-
-  -- TODO: 3. Save collected data to FIFO, 
-  -- 4. Toggle FIO1 line to indicate that a trigger occured.
-  MB.W(2001,0,1)
-  MB.W(2001,0,FIO1_INIT_STATE)
-
-  --Wait for the trigger signal to re-set.
-  state = TRIG_RESET_STATE
-elseif state = TRIG_RESET_STATE then
-  -- Wait for trigger to re-set
-  local fio0 = MB.R(2000, 0)
-  if fio0 == 0 then
+  if state == INIT_STATE then
+    -- Initializing
+    -- 1. Send 6 bytes of data to a SPI slave device.
+    MB.W(5009,0,table.getn(configBytes))
+    MB.WA(5010,99,table.getn(configBytes),configBytes)
+    MB.W(5007,0,1)
+  
     --Start waiting for a trigger to occur.
     state = WAIT_TRIG_STATE
+  elseif state == WAIT_TRIG_STATE then
+    -- Waiting for trigger
+    local fio0 = MB.R(2000, 0)
+    if fio0 == 1 then
+      curNum = curNum +1
+      if curNum >= numReq then
+        --Switch to the acquisitino state.
+        state = ACQ_STATE
+      end
+    else
+      curNum = 0
+    end
+  elseif state == ACQ_STATE then
+    -- Acquire data from SPI device
+    -- 1. Send 6 bytes of data to a SPI slave device.
+    MB.W(5009,0,table.getn(acqStateWriteData))
+    MB.WA(5010,99,table.getn(acqStateWriteData),acqStateWriteData)
+    MB.W(5007,0,1)
+  
+    -- 2. Read 9 bytes of data from a SPI slave device.
+    MB.W(5009,0,acqStateNumBytesToRead)
+    MB.WA(5010,99,acqStateNumBytesToRead,dummyWriteDataForRead)
+    MB.W(5007,0,1)
+    local readData = MB.RA(5050, 99)
+  
+    -- TODO: 3. Save collected data to FIFO
+    
+    
+    -- 4. Toggle FIO1 line to indicate that a trigger occured.
+    MB.W(2001,0,1)
+    MB.W(2001,0,FIO1_INIT_STATE)
+  
+    --Wait for the trigger signal to re-set.
+    state = TRIG_RESET_STATE
+  elseif state == TRIG_RESET_STATE then
+    -- Wait for trigger to re-set
+    local fio0 = MB.R(2000, 0)
+    
+    if fio0 == 0 then
+      curNum = curNum +1
+      if curNum >= numReq then
+        --Start waiting for a trigger to occur.
+        state = WAIT_TRIG_STATE
+      end
+    else
+      curNum = 0
+    end
+  else 
+    print("In Else case.......", state)
   end
-else 
-  print("In Else case.......", state)
 end
